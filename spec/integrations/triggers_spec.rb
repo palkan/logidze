0a4151a66db8c373f893dc9e6a6ebe093bc5ab3f
Hung Dao <mephis1987@gmail.com>
2018-09-25 15:39:41 -0700

    even more tests

--- spec/integrations/triggers_spec.rb
+++ spec/integrations/triggers_spec.rb
@@ -491,7 +491,35 @@ describe "Logidze triggers", :db do
       Post.instance_variable_set(:@attribute_names, nil)
     end
 
-    it "stores limited number of logs", :aggregate_failures do
+    it 'does not merge the logs outside debounce_time' do
+      post = nil
+      Timecop.freeze(Time.at(1_000_000)) do
+        post = Post.create!(title: 'Triggers', rating: 10)
+      end
+      Timecop.freeze(Time.at(1_000_100)) do
+        post.update!(rating: 100)
+      end
+      expect(post.reload.log_version).to eq 2
+      expect(post.log_size).to eq 2
+      expect(post.log_data.versions.last.changes)
+        .to_not include("title" => 'Triggers')
+    end
+
+    it 'merges the logs within debounce_time' do
+      post = nil
+      Timecop.freeze(Time.at(1_000_000)) do
+        post = Post.create!(title: 'Triggers', rating: 10)
+      end
+      Timecop.freeze(Time.at(1_000_001)) do
+        post.update!(rating: 100)
+      end
+      expect(post.reload.log_version).to eq 2
+      expect(post.log_size).to eq 1
+      expect(post.log_data.versions.last.changes)
+        .to include("title" => "Triggers", "rating" => 100)
+    end
+
+    it "merges the logs within timeline", :aggregate_failures do
       post = nil
       Timecop.freeze(Time.at(1_000_000)) do
         post = Post.create!(title: 'Triggers', rating: 10)
