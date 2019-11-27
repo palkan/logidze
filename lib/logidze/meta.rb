123f334c473502a52a7610ef04a6858f3f7b9bfd
Oleg Kiviljov <oleg.kiviljov@gmail.com>
2019-11-27 10:53:37 +0200

    Raise ArgumentError if with_meta was called without block

--- lib/logidze/meta.rb
+++ lib/logidze/meta.rb
@@ -30,7 +30,7 @@ module Logidze # :nodoc:
       end
 
       def perform
-        return if block.nil?
+        raise ArgumentError, 'Block must be given' unless block
         return block.call if meta.nil?
 
         call_block_in_meta_context
