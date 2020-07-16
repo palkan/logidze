421575ca057119059751c16e12e7392fa0960038
Vladimir Dementyev <dementiev.vm@gmail.com>
2020-07-16 12:47:58 +0300

    Add comment to trigger definition

--- lib/generators/logidze/model/triggers/logidze.sql
+++ lib/generators/logidze/model/triggers/logidze.sql
@@ -1,4 +1,6 @@
 CREATE TRIGGER logidze_on_<%= table_name %>
 BEFORE UPDATE OR INSERT ON <%= table_name %> FOR EACH ROW
 WHEN (coalesce(current_setting('logidze.disabled', true), '') <> 'on')
+-- Parameters: history_size_limit (integer), timestamp_column (text), filtered_columns (text[]),
+-- include_columns (boolean), debounce_time_ms (integer)
 EXECUTE PROCEDURE logidze_logger(<%= logidze_logger_parameters %>);
