96dc7a78bf8b683a78495afeef04308b14789ffd
Vladimir Dementyev <dementiev.vm@gmail.com>
2020-07-22 13:46:10 +0300

    Fix generated migrations indentation

--- lib/generators/logidze/inject_sql.rb
+++ lib/generators/logidze/inject_sql.rb
@@ -3,12 +3,15 @@
 module Logidze
   module Generators
     module InjectSql
-      def inject_sql(source)
+      def inject_sql(source, indent: 4)
         source = ::File.expand_path(find_in_source_paths(source.to_s))
 
-        ERB.new(::File.binread(source)).tap do |erb|
-          erb.filename = source
-        end.result(instance_eval("binding")) # rubocop:disable Style/EvalWithLocation
+        indent(
+          ERB.new(::File.binread(source)).tap do |erb|
+            erb.filename = source
+          end.result(instance_eval("binding")), # rubocop:disable Style/EvalWithLocation
+          indent
+        )
       end
     end
   end
