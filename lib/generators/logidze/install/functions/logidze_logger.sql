4cd64e57236afb94b5d0d4ddff381b4d39785f0c
SupDex <symeton@gmail.com>
2023-02-06 12:54:37 +0300

    pg 9.6 and 10 do not have OLD variable in INSERT

--- lib/generators/logidze/install/functions/logidze_logger.sql
+++ lib/generators/logidze/install/functions/logidze_logger.sql
@@ -3,8 +3,15 @@ CREATE OR REPLACE FUNCTION logidze_logger() RETURNS TRIGGER AS $body$
   DECLARE
     updated_record record;
   BEGIN
+    IF TG_OP = 'INSERT' THEN
+      updated_record := logidze_generate_log_data(NEW, null, TG_OP, TG_ARGV);
 
-    IF TG_OP = 'INSERT' OR TG_OP = 'UPDATE' THEN
+      IF updated_record.log_data::text IS DISTINCT FROM NEW.log_data::text THEN
+        NEW.log_data := updated_record.log_data;
+      END if;
+    END IF;
+
+    IF TG_OP = 'UPDATE' THEN
       updated_record := logidze_generate_log_data(NEW, OLD, TG_OP, TG_ARGV);
 
       IF updated_record.log_data::text IS DISTINCT FROM NEW.log_data::text THEN
