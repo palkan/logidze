fa7d1b6103538b7dfd6ed6c7c72438afb8b007f2
SupDex <symeton@gmail.com>
2023-02-06 11:44:10 +0300

    add check for version of PG in migration

--- spec/dummy/db/migrate/20230204194940_create_partitioned_posts.rb
+++ spec/dummy/db/migrate/20230204194940_create_partitioned_posts.rb
@@ -1,5 +1,17 @@
 class CreatePartitionedPosts < ActiveRecord::Migration[6.1]
+  PG_MINIMUM_VERSION = 11
+
   def up
+    current_pg_version =
+      ActiveRecord::Base
+        .connection
+        .execute("SELECT (substr(current_setting('server_version'), 1, 2)::smallint);")
+        .values.first&.first
+
+    unless pg_version_11_and_upper?(current_pg_version)
+      return false
+    end
+
     execute <<-SQL
       CREATE TABLE partitioned_posts (
         id         SERIAL,
@@ -12,26 +24,29 @@ class CreatePartitionedPosts < ActiveRecord::Migration[6.1]
         created_at timestamp(6) without time zone NOT NULL,
         updated_at timestamp(6) without time zone NOT NULL
       ) PARTITION BY RANGE (logdate);
+      
+      ALTER TABLE partitioned_posts
+        ADD CONSTRAINT partitioned_posts_pkey PRIMARY KEY (id, logdate);
 
       CREATE TABLE partitioned_posts_y2023m01 PARTITION OF partitioned_posts
         FOR VALUES FROM ('2023-01-01') TO ('2023-02-01');
-      
-      ALTER TABLE partitioned_posts_y2023m01
-        ADD CONSTRAINT partitioned_posts_y2023m01_pkey PRIMARY KEY (id, logdate);
 
       CREATE TABLE partitioned_posts_y2023m02 PARTITION OF partitioned_posts
         FOR VALUES FROM ('2023-02-01') TO ('2023-03-01');
-
-      ALTER TABLE partitioned_posts_y2023m02
-        ADD CONSTRAINT partitioned_posts_y2023m02_pkey PRIMARY KEY (id, logdate);
     SQL
   end
 
   def down
     execute <<-SQL
-      DROP TABLE partitioned_posts_y2023m02;
-      DROP TABLE partitioned_posts_y2023m01;
-      DROP TABLE partitioned_posts;
+      DROP TABLE IF EXISTS partitioned_posts_y2023m02;
+      DROP TABLE IF EXISTS partitioned_posts_y2023m01;
+      DROP TABLE IF EXISTS partitioned_posts;
     SQL
+  end
+
+  private
+
+  def pg_version_11_and_upper?(current_version)
+    current_version >= PG_MINIMUM_VERSION
   end
 end
