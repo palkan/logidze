59218007c0d7e9aecd551a9b70c5dd0aeae9de5b
SparLaimor <sparlaimor@gmail.com>
2023-02-07 01:45:08 +0300

    Add partition table tests
    (cherry picked from commit 40043f211c137c67607b4631927cb2609ce93e04)

--- spec/integration/partition_spec.rb
+++ spec/integration/partition_spec.rb
@@ -2,62 +2,64 @@
 
 require "acceptance_helper"
 
-describe "partition change imitation", :db do
-  include_context "cleanup migrations"
+describe "partition change", :db do
+  context "partitioned by age" do
+    include_context "cleanup migrations"
 
-  before(:all) do
-    Dir.chdir("#{File.dirname(__FILE__)}/../dummy") do
-      successfully "rails generate logidze:model post --after_trigger"
-      successfully "rake db:migrate"
+    before(:all) do
+      skip_database_versions(0...12_00_00)
 
-      # Close active connections to handle db variables
-      ActiveRecord::Base.connection_pool.disconnect!
-    end
+      Dir.chdir("#{File.dirname(__FILE__)}/../dummy") do
+        successfully "rails generate logidze:model partitioned_user --after_trigger"
+        successfully "rake db:migrate"
 
-    Post.reset_column_information
-  end
-
-  describe "update" do
-    let(:initial_log_data) { Post.create!(title: "Partition Post", rating: 5, active: true).reload.log_data }
-
-    it "creates a new version like a full snapshot", :aggregate_failures do
-      post = Post.create!(title: "Partition Post", active: true, rating: 25, log_data: initial_log_data)
-
-      expect(post.reload.log_version).to eq 2
-      expect(post.log_data.versions.last.changes)
-        .to include("title" => "Partition Post", "rating" => 25, "active" => true)
-
-      post.update!(rating: 30)
-
-      expect(post.reload.log_version).to eq 3
-      expect(post.log_data.versions.last.changes)
-        .to include("rating" => 30)
-      expect(post.log_data.versions.last.changes.keys)
-        .not_to include("title", "active")
-    end
-  end
-
-  describe "timestamp_column is not set" do
-    let(:initial_log_data) { Post.create!(rating: 10, updated_at: Time.at(0)).reload.log_data }
-
-    it "uses 'updated_at' column if it exists", :aggregate_failures do
-      post = nil
-      Timecop.freeze(Time.at(1_000_000)) do
-        post = Post.create!(rating: 20, log_data: initial_log_data)
+        # Close active connections to handle db variables
+        ActiveRecord::Base.connection_pool.disconnect!
       end
 
-      expect(post.reload.log_version).to eq 2
-      expect(post).to use_timestamp(:updated_at)
+      PartitionedUser.reset_column_information
     end
 
-    it "sets version timestamp to statement_timestamp() if 'updated_at' did not change", :aggregate_failures do
-      post = nil
-      Timecop.freeze(Time.at(0)) do
-        post = Post.create!(rating: 20, log_data: initial_log_data)
+    describe "update" do
+      let(:partitioned_user) { PartitionedUser.create!(name: "Partition User", age: 5, active: true).reload }
+
+      it "creates a new version like a full snapshot", :aggregate_failures do
+        expect(partitioned_user.log_version).to eq 1
+
+        partitioned_user.update!(age: 25)
+
+        expect(partitioned_user.reload.log_version).to eq 2
+        expect(partitioned_user.log_data.versions.last.changes)
+          .to include("name" => "Partition User", "age" => 25, "active" => true)
+
+        partitioned_user.update!(age: 30)
+
+        expect(partitioned_user.reload.log_version).to eq 3
+        expect(partitioned_user.log_data.versions.last.changes)
+          .to include("age" => 30)
+        expect(partitioned_user.log_data.versions.last.changes.keys)
+          .not_to include("tittle", "active")
+      end
+    end
+
+    describe "timestamp_column is not set" do
+      let(:partitioned_user) { PartitionedUser.create!(age: 10, updated_at: Time.at(0)).reload }
+
+      it "uses 'updated_at' column if it exists", :aggregate_failures do
+        Timecop.freeze(Time.at(1_000_000)) do
+          partitioned_user.update!(age: 20)
+        end
+
+        expect(partitioned_user.reload).to use_timestamp(:updated_at)
       end
 
-      expect(post.reload.log_version).to eq 2
-      expect(post).to use_statement_timestamp
+      it "sets version timestamp to statement_timestamp() if 'updated_at' did not change", :aggregate_failures do
+        Timecop.freeze(Time.at(0)) do
+          partitioned_user.update!(age: 20)
+        end
+
+        expect(partitioned_user.reload).to use_statement_timestamp
+      end
     end
   end
 end
