8ce56bbb4fe97efbb574b22e85f58827b39a959c
SupDex <symeton@gmail.com>
2023-02-10 19:42:09 +0300

    rename constant in spec PgVersionHelpers

--- spec/support/pg_versions_helpers.rb
+++ spec/support/pg_versions_helpers.rb
@@ -3,11 +3,11 @@
 module Logidze
   module PgVersionsHelpers #:nodoc:
     PG_MINIMUM_VERSION = 11
-    SKIP_POSTGRES_V11_12_MESSAGE = "Skipped specs for PG 11 version and upper, because current PG VERSION is another".freeze
+    SKIP_POSTGRES_LESS_11V_MESSAGE = "Skipped specs for PG 11 version and upper, because current PG VERSION is another".freeze
 
     def only_for_pg_version_11_and_upper(current_pg_version)
       if current_pg_version < PG_MINIMUM_VERSION
-        return skip(SKIP_POSTGRES_V11_12_MESSAGE)
+        return skip(SKIP_POSTGRES_LESS_11V_MESSAGE)
       end
 
       yield
