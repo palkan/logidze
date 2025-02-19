564c944e70598a655a10fed70b7f47c4a7414064
lokideos <archmage@hey.com>
2025-02-19 19:30:31 +0600

    fix: fix issue with long index name

--- spec/dummy/db/migrate/20250216095430_create_logidze_data.rb
+++ spec/dummy/db/migrate/20250216095430_create_logidze_data.rb
@@ -4,9 +4,11 @@ class CreateLogidzeData < ActiveRecord::Migration[5.0]
   def change
     create_table :logidze_data do |t|
       t.jsonb :log_data
-      t.belongs_to :loggable, polymorphic: true
+      t.belongs_to :loggable, polymorphic: true, index: false
 
       t.timestamps
+
+      t.index %w[loggable_type loggable_id], name: "index_logidze_loggable"
     end
   end
 end
